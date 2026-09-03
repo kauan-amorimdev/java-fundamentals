ac52e3230123d3132da9653a1400491927c5a6c2
Dev Kauã <269943517+kauan-amorimdev@users.noreply.github.com>
2026-09-03 17:43:24 -0300

    "practice constructors with Aluno"

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,24 +1,22 @@
 public class Aluno {
-   private String nome;
-    private double nota;
+    private String matricula;
+    private String nome;
 
-    public Aluno(String nome, double nota){
-        this.nome = nome;
-        this.nota = nota;
+    public Aluno(){
+        this("Desconhecido", "Sem matricula");
     }
 
+    public Aluno(String nome){
+        this(nome, "Sem matricula");
+    }
+    public Aluno(String nome, String matricula){
+        this.nome = nome;
+        this.matricula = matricula;
+    }
     public String getNome(){
         return nome;
     }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-    public double getNota(){
-        return nota;
-    }
-    public void setNota(double nota){
-        if (nota >= 0 && nota <= 10) {
-            this.nota = nota;
-        }
+    public String getMatricula(){
+        return matricula;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args){
-    Conta conta1 = new Conta();
-    Conta conta2 = new Conta("kauan");
-    Conta conta3 = new Conta("kauan", 2000.0);
-    mostrar(conta1, conta2, conta3);
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno("kauan");
+        Aluno aluno3 = new Aluno("kauan", "123456");
+        mostrar(aluno1, aluno2, aluno3);
     }
-
-    public static void mostrar(Conta conta1, Conta conta2, Conta conta3){
-        System.out.println("Titular: " + conta1.getTitular() + " Saldo: R$ " + conta1.getSaldo());
-        System.out.println("Titular: " + conta2.getTitular() + " Saldo: R$" + conta2.getSaldo());
-        System.out.println("Titular: " + conta3.getTitular() + " Saldo: R$" + conta3.getSaldo());
+    public static void mostrar(Aluno aluno1, Aluno aluno2, Aluno aluno3){
+        System.out.println("Nome: " + aluno1.getNome() + " Matricula: " + aluno1.getMatricula());
+        System.out.println("Nome: " + aluno2.getNome() + " Matricula: " + aluno2.getMatricula());
+        System.out.println("Nome: " + aluno3.getNome() + " Matricula: " + aluno3.getMatricula());
     }
 }
